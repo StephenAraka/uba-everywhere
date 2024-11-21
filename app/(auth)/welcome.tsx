225e3d1af2cf971015d2f794ae56906743096493
StephenAraka <stevenaraka@gmail.com>
2024-11-21 09:39:59 +0300

    Remove auth from root folder

---
+++ app/(auth)/welcome.tsx
@@ -0,0 +1,12 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Onboarding = () => {
+  return (
+    <SafeAreaView className="flex-1 h-full items-center justify-between bg-white">
+      <Text>Onboarding</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Onboarding;
