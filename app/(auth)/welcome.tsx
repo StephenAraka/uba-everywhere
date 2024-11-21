062ffe292f3220ffde97ee7e119e6728e2957d25
StephenAraka <stevenaraka@gmail.com>
2024-11-21 10:02:59 +0300

    Add skip onboarding button

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,10 +1,16 @@
-import { Text } from "react-native";
+import { router } from "expo-router";
+import { Text, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const Onboarding = () => {
   return (
     <SafeAreaView className="flex-1 h-full items-center justify-between bg-white">
-      <Text>Onboarding</Text>
+      <TouchableOpacity
+        onPress={() => router.replace("/(auth)/sign-up")}
+        className="w-full flex justify-end items-end p-5"
+      >
+        <Text className="text-black text-md font-JakartaBold">Skip</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 };
