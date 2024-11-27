b7a48038d8d5ebb34f2e6c978360dc34f531adce
StephenAraka <stevenaraka@gmail.com>
2024-11-27 23:49:54 +0300

    Configure token cache

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -13,6 +13,7 @@ import "react-native-reanimated";
 import "../global.css";
 
 import { ClerkProvider, ClerkLoaded } from "@clerk/clerk-expo";
+import { Slot } from "expo-router";
 
 const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
 
