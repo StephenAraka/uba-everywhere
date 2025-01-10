c5815f45632922c4eff66553178ce9ade65af7dc
StephenAraka <stevenaraka@gmail.com>
2025-01-10 13:01:36 +0300

    A lil sth sth

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,10 +1,12 @@
-import { Text, View } from "react-native";
+import { useUser } from "@clerk/clerk-expo";
+import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const Home = () => {
+  const { user } = useUser();
   return (
     <SafeAreaView>
-      <Text>Home</Text>
+      <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
     </SafeAreaView>
   );
 };
